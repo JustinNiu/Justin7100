1eace02bce496e25579d9c62a85121b3d4ff1490
JustinNiu <buptnsy@gmail.com>
2016-04-14 08:59:22 -0400

    update on platform android

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,6 +16,85 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.controller('SoundBoardCtrl', function ($scope, $window) {
+
+  $scope.media = null;
+
+  $scope.model = {
+    showDelete: false,
+    showMove: false,
+    sounds: [
+      {
+        'title': 'moca',
+        'note': 'moca',
+        'file': 'mp3/defe.wav'
+      },
+      {
+        'title': 'coo',
+        'note': 'coo',
+        'file': 'mp3/dob.wav'
+      },
+      {
+        'title': 'horst',
+        'note': 'horst',
+        'file': 'mp3/han.wav'
+      },
+      {
+        'title': 'oma',
+        'note': 'oma',
+        'file': 'mp3/hit.wav'
+      },
+      {
+        'title': 'tair',
+        'note': 'tair',
+        'file': 'mp3/jug.wav'      },
+      {
+        'title': 'zish',
+        'note': 'zish',
+        'file': 'mp3/wah.wav'
+      }
+
+    ]
+  };
+
+  $scope.deleteSound = function ($index) {
+    $scope.model.sounds.splice($index, 1);
+  };
+
+  $scope.moveSound = function (sound, fromIndex, toIndex) {
+    $scope.model.sounds.splice(fromIndex, 1);
+    $scope.model.sounds.splice(toIndex, 0 , sound);
+  };
+
+  $scope.play = function(sound){
+    if ($scope.media) {
+      $scope.media.pause();
+    }
+
+    if ($window.cordova){
+        ionic.Platform.ready(function () {
+        var src = sound.file;
+
+        // if (ionic.Platform.is('android')) {
+        //   src = '/android_asset/www' + src;
+        // }
+
+        $scope.media = new $window.Media(src);
+        $scope.media.play();
+      });
+    }else{
+      $scope.media = new Audio();
+      $scope.media.src = sound.file;
+      $scope.media.load();
+      $scope.media.play();
+    }
 
 
+
+  }
+
+
+
+});
--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,43 +16,85 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.controller('SoundBoardCtrl', function ($scope, $window) {
+
+  $scope.media = null;
+
+  $scope.model = {
+    showDelete: false,
+    showMove: false,
+    sounds: [
+      {
+        'title': 'moca',
+        'note': 'moca',
+        'file': 'mp3/defe.wav'
+      },
+      {
+        'title': 'coo',
+        'note': 'coo',
+        'file': 'mp3/dob.wav'
+      },
+      {
+        'title': 'horst',
+        'note': 'horst',
+        'file': 'mp3/han.wav'
+      },
+      {
+        'title': 'oma',
+        'note': 'oma',
+        'file': 'mp3/hit.wav'
+      },
+      {
+        'title': 'tair',
+        'note': 'tair',
+        'file': 'mp3/jug.wav'      },
+      {
+        'title': 'zish',
+        'note': 'zish',
+        'file': 'mp3/wah.wav'
+      }
+
+    ]
+  };
+
+  $scope.deleteSound = function ($index) {
+    $scope.model.sounds.splice($index, 1);
+  };
+
+  $scope.moveSound = function (sound, fromIndex, toIndex) {
+    $scope.model.sounds.splice(fromIndex, 1);
+    $scope.model.sounds.splice(toIndex, 0 , sound);
+  };
+
+  $scope.play = function(sound){
+    if ($scope.media) {
+      $scope.media.pause();
+    }
+
+    if ($window.cordova){
+        ionic.Platform.ready(function () {
+        var src = sound.file;
+
+        // if (ionic.Platform.is('android')) {
+        //   src = '/android_asset/www' + src;
+        // }
+
+        $scope.media = new $window.Media(src);
+        $scope.media.play();
+      });
+    }else{
+      $scope.media = new Audio();
+      $scope.media.src = sound.file;
+      $scope.media.load();
+      $scope.media.play();
+    }
 
 
-// app.controller('JustinProject',function($scope){
 
-//   var pong = new Howl({urls: ['http://www.javascriptoo.com/application/html/pong.wav']})
-//   document.getElementById('btn').onclick=function(){pong.play()}
-
-//   var pong3 = new Howl({
-//   urls: ["mp3/oboe.wav"],
-//   sprite: {
-//     blast: [0, 2000],
-//     laser: [300, 7000],
-//     winn: [5000, 100]
-//     }
-//   });
-
-//   document.getElementById('btn3').onclick=function(){pong3.play('winn')}
-// }
-
-
-//   var pong2 = new Howl({
-//     // urls: ['http://www.javascriptoo.com/application/html/kingsolomonhill-TimesHasDoneGotHard-short.mp3'],
-//   urls:["mp3/oboe.wav"],
-//   loop: true,
-//   volume: 0.5,
-//   onend: function() {
-//     alert('Finished!');
-//   }
-// });
-//   document.getElementById('btn2').onclick=function(){pong2.play()};
-
-//     // document.getElementById('btn21').onclick=function(){
-//     //   pong2.pause().fadeIn(0.5, 2000);
-//     // });
-
-  // )
+  }
 
 
 
+});
